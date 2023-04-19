232cd657d5807ae0a3b1adc0634b33ac0ae5dad5
jaimelop83 <jaime.a.lopez@gmail.com>
2023-04-19 15:27:00 -0400

    Added AllProducts Component - Please Review

--- client/features/allProducts/AllProducts.js
+++ client/features/allProducts/AllProducts.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProducts } from "./allProductsSlice";
+
+
 
 export const AllProducts = () => {
-  return <div>AllProducts</div>;
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.allProducts.products); // state.allProducts.products is the name of the slice of state in the store
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+    
+  return (
+    <div>
+      <h1>AllProducts</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+export default AllProducts;
