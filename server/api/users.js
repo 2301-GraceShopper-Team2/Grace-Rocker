436b2698e88a75c7be876127370ed9e8b59a5584
jackconwayiv <jackconwayiv@gmail.com>
2023-04-19 09:36:25 -0700

    user endpoints

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {
-  models: { User, Order, Product, Order_Products },
+  models: { User, Order },
 } = require("../db");
 module.exports = router;
 
@@ -29,3 +29,38 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  //requires auth
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    const updatedUser = await user.update(req.body);
+    res.json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    await User.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
