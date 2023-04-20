d5066764978685f0e593c3581b12c3a221bfef0f
jaimelop83 <jaime.a.lopez@gmail.com>
2023-04-20 12:16:19 -0400

    Added singleProductSlice.js

---
+++ client/features/singleProduct/singleProductSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchSingleProductAsync = createAsyncThunk(
+    "singleProduct/fetchSingleProduct",
+    async (id) => {
+        try {
+            const { data } = await axios.get(`/api/products/${id}`);
+            return data;
+        } catch (err) {
+            return err;
+        }
+    }
+);
+
+export const singleProductSlice = createSlice({
+    name: "singleProduct",
+    initialState: {},
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchSingleProductAsync.fulfilled, (state, action) => {
+            return action.payload;
+        });
+    }
+});
+
+export const selectSingleProduct = (state) => state.singleProduct;
+
+export default singleProductSlice.reducer;
+
+// Path: client/features/singleProduct/SingleProduct.js
