62e1b02f7037c89cd28c2ad8344440053f880a94
jaimelop83 <jaime.a.lopez@gmail.com>
2023-04-24 14:59:55 -0400

    updated Cart for guest

--- client/features/cart/cartSlice.js
+++ client/features/cart/cartSlice.js
@@ -19,12 +19,33 @@ export const fetchCartAsync = createAsyncThunk(
 
 export const addProductToCartAsync = createAsyncThunk(
   "cart/addProductToCart",
-  async (productId) => {
-    try {
-      const { data } = await axios.post("/api/cart", { productId });
-      return data;
-    } catch (err) {
-      return err;
+  async (productId, thunkAPI) => {
+    const me = thunkAPI.getState().auth.me;
+
+    if (me && me.id) {
+      try {
+        const { data } = await axios.post(`/api/cart/${me.id}/product/${productId}`);
+        return data;
+      } catch (err) {
+        return err;
+      }
+    } else {
+      // handle guest cart
+      const guestCart = JSON.parse(localStorage.getItem("guestCart")) || [];
+      const existingProduct = guestCart.find((item) => item.id === productId);
+
+      if (existingProduct) {
+        existingProduct.order_products.quantity += 1;
+      } else {
+        const { data: product } = await axios.get(`/api/products/${productId}`);
+        product.order_products = { quantity: 1 };
+        guestCart.push(product);
+      }
+      // update guest cart in localstorage
+      localStorage.setItem("guestCart", JSON.stringify(guestCart));
+
+      // return the guest cart to update the Redux state
+      return guestCart;
     }
   },
 );
@@ -41,16 +62,31 @@ export const deleteProductFromCartAsync = createAsyncThunk(
   },
 );
 
+const initialState = {
+  userCart: [],
+  guestCart: [],
+};
+
 export const cartSlice = createSlice({
   name: "cart",
-  initialState: [],
-  reducers: {},
+  initialState,
+  reducers: {
+    setGuestCart: (state, action) => {
+      state.guestCart = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchCartAsync.fulfilled, (state, action) => {
       return action.payload;
     });
     builder.addCase(addProductToCartAsync.fulfilled, (state, action) => {
-      state.push(action.payload);
+      if (Array.isArray(action.payload)) {
+        // Guest Cart
+        state.guestCart = action.payload;
+      } else {
+        // User Cart - logged in
+        state.userCart.push(action.payload);
+      }
     });
     builder.addCase(deleteProductFromCartAsync.fulfilled, (state, action) => {
       return state.filter((product) => product.id != action.payload);
@@ -58,7 +94,13 @@ export const cartSlice = createSlice({
   },
 });
 
-export const selectCart = (state) => state.cart;
+export const selectCart = (state) => {
+  if (state.auth.me && state.auth.me.id) {
+    return state.cart.userCart;
+  } else {
+    return state.cart.guestCart;
+  }
+};
 
 export default cartSlice.reducer;
 
