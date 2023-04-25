864699b4238496a85d9c502df6a2d5e9a695eef7
Peter Wagner <pwagnermusic@gmail.com>
2023-04-25 17:05:45 -0400

    configured css bootstrap

--- client/features/singleProduct/SingleProduct.js
+++ client/features/singleProduct/SingleProduct.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
-import { editProductAsync, selectSingleProduct } from "./singleProductSlice";
-import { fetchSingleProductAsync } from "./singleProductSlice";
-import { deleteProductAsync } from "../allProducts/allProductsSlice";
-import { addProductToCartAsync } from "../cart/cartSlice";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { editProductAsync, selectSingleProduct } from './singleProductSlice';
+import { fetchSingleProductAsync } from './singleProductSlice';
+import { deleteProductAsync } from '../allProducts/allProductsSlice';
+import { addProductToCartAsync } from '../cart/cartSlice';
 
 const SingleProduct = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [SKU, setSKU] = useState("");
-  const [price, setPrice] = useState("");
-  const [imageURL, setImageURL] = useState("");
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [SKU, setSKU] = useState('');
+  const [price, setPrice] = useState('');
+  const [imageURL, setImageURL] = useState('');
   const [numItems, setNumItems] = useState(1);
 
   const isAdmin = useSelector((state) => state.auth.me.isAdmin);
@@ -36,10 +36,10 @@ const SingleProduct = () => {
     if (SKU) update.SKU = SKU;
     if (imageURL) update.imageURL = imageURL;
     dispatch(editProductAsync(update));
-    setName("");
-    setDescription("");
-    setSKU("");
-    setPrice("");
+    setName('');
+    setDescription('');
+    setSKU('');
+    setPrice('');
     setImageURL();
   };
 
@@ -111,9 +111,8 @@ const SingleProduct = () => {
         </div>
       )}
 
-     
-
       <button
+        className="btn btn-primary"
         onClick={() => {
           dispatch(addProductToCartAsync(id));
         }}
@@ -125,7 +124,7 @@ const SingleProduct = () => {
         <button
           onClick={() => {
             dispatch(deleteProductAsync(id));
-            navigate("/products");
+            navigate('/products');
           }}
         >
           Delete Product
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,28 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [require('autoprefixer')],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
     ],
   },
 };
