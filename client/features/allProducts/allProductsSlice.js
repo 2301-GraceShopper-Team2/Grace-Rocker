734849e6e13fd0226eacdbd923b0f2d8d2b17cf5
Jake Alagel <alagel.jacob@gmail.com>
2023-04-25 21:51:48 -0400

    pray24

--- client/features/allProducts/allProductsSlice.js
+++ client/features/allProducts/allProductsSlice.js
@@ -1,19 +1,17 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { apiUrl } from "../../config";
 
 export const fetchAllProductsAsync = createAsyncThunk(
   "products/fetchAll",
   async () => {
     try {
-      const { data } = await axios.get(`${apiUrl}/api/products`);
+      const { data } = await axios.get("/api/products");
       return data;
     } catch (err) {
       return err;
     }
   }
 );
-
 export const addProductAsync = createAsyncThunk(
   "products/addProduct",
   async ({ name, description, price, imageURL, SKU }) => {
